c0e79c3e4cfb18f1e51f5fc698175965c816db45
Safaa Jassim <safajassem1234@gmail.com>
2023-06-10 20:15:56 +0300

    Add Indicator Loading

--- UnInfoApp/MainPage.xaml.cs
+++ UnInfoApp/MainPage.xaml.cs
@@ -19,19 +19,22 @@ public partial class MainPage : ContentPage
     {
         if (countryName.Text != null)
         {
+
             LoadData(countryName.Text);
+
 
         }
     }
 
     private async void LoadData(string countryName)
     {
+
         // Clear
         rootobjects.Clear();
         ConLayout.Clear();
-
         // Get Respone and Fill root UN
-        var respone=await httpHelper.GetResponeAsync("http://universities.hipolabs.com/search?country="+ countryName);
+        IndicatorLoading.IsRunning = true; IndicatorLoading.IsVisible = true;
+        var respone =await httpHelper.GetResponeAsync("http://universities.hipolabs.com/search?country="+ countryName);
         rootobjects=Newtonsoft.Json.JsonConvert.DeserializeObject<List<Rootobject>>(respone); 
         // Foor loop into rootobjetcs
         for (int i = 0; i < rootobjects.Count; i++)
@@ -39,6 +42,8 @@ public partial class MainPage : ContentPage
             var item = rootobjects[i];
             ConLayout.Add(new Views.UnInfoView(item.name, item.web_pages[0]));
         }
+        IndicatorLoading.IsRunning = false; IndicatorLoading.IsVisible = false;
+
     }
 }
 
